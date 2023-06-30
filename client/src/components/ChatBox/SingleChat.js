593924fd4f6a34a591831b09c08bf5a838e84fee
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2023-06-30 18:39:20 +0500

    Added Layout for rendering messages and created handler functions

--- client/src/components/ChatBox/SingleChat.js
+++ client/src/components/ChatBox/SingleChat.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { ChatState } from '../../context/chatProvider';
-import { Box, IconButton, Text, Stack, Avatar } from '@chakra-ui/react';
+import { Box, IconButton, Text, Stack, Avatar, Spinner, FormControl, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
 import { ArrowBackIcon, InfoIcon } from '@chakra-ui/icons';
 import ProfileModal from '../ProfileModal';
 import ProfileGroupModal from '../ProfileGroupModal';
+import { useState } from 'react';
+import { ArrowRightIcon } from '@chakra-ui/icons';
+import axios from 'axios';
+import { useToast } from '@chakra-ui/react';
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
     const { user, selectedChat, setSelectedChat } = ChatState();
+    const [messages, setMessages] = React.useState([]);
+    const [loading, setLoading] = React.useState(false);
+    const [newMessage, setNewMessage] = useState('')
+    const toast = useToast();
 
     const getSender = (loggedUser, users) => {
 
@@ -26,10 +34,50 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
         return '';
     };
 
+    const sendMessage = async (e) => {
+        if (e.key === 'Enter' && newMessage) {
+            try {
+
+                setLoading(true);
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                }
+
+                const { data } = await axios.post('/api/message/send', {
+                    content: newMessage,
+                    chatId: selectedChat._id,
+                }, config);
+
+                setNewMessage('');
+                setMessages([...messages, data]);
+                setLoading(false);
+            }
+            catch (err) {
+                toast({
+                    title: "Unable to Load Chat Messages",
+                    description: "Unable to send message.",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                })
+                setLoading(false);
+            }
+        }
+    }
+    const typingHandler = (e) => {
+        setNewMessage(e.target.value);
+        //Typing Indicator Logic
+    }
+
     return (
         <>
             {
-                selectedChat ? <Box
+                selectedChat ? <Box display='flex' w='100%' flexDirection='column' h='100%'
+
+                > <Box
                     display='flex'
                     py={3}
                     px={3}
@@ -43,57 +91,77 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                     opacity={1.0}
                     height='65px'
                 >
-                    <IconButton
-                        display={{ base: 'flex', md: 'none' }}
-                        aria-label="Back to chats list"
-                        icon={<ArrowBackIcon />}
-                        onClick={() => setSelectedChat("")}
-                        bg='transparent'
-                        _hover={{ bg: 'transparent' }}
-                        _active={{ bg: 'transparent' }}
-                        variant='outline'
-                        color='white'
-                    />
-                    <Stack direction='row' alignItems='center' color='white' >
+                        <IconButton
+                            display={{ base: 'flex', md: 'none' }}
+                            aria-label="Back to chats list"
+                            icon={<ArrowBackIcon />}
+                            onClick={() => setSelectedChat("")}
+                            bg='transparent'
+                            _hover={{ bg: 'transparent' }}
+                            _active={{ bg: 'transparent' }}
+                            variant='outline'
+                            color='white'
+                        />
+                        <Stack direction='row' alignItems='center' color='white' >
 
 
-                        {!selectedChat.isGroupChat ?
-                            (<>
-                                <Avatar name={getSender(user, selectedChat.users)} src={getSender(user, selectedChat.users).profilePic} />
-                                <Text>
-                                    {getSender(user, selectedChat.users)}
-                                </Text>
-                            </>) :
-
-                            (
-                                <>
-                                    <Avatar name={selectedChat.chatName} />
+                            {!selectedChat.isGroupChat ?
+                                (<>
+                                    <Avatar name={getSender(user, selectedChat.users)} src={getSender(user, selectedChat.users).profilePic} />
                                     <Text>
-                                        {selectedChat.chatName.toUpperCase()}
+                                        {getSender(user, selectedChat.users)}
                                     </Text>
-                                </>
-                            )
-                        }
-                    </Stack>
+                                </>) :
 
-                    {
-                        !selectedChat.isGroupChat ? (<ProfileModal user={getSenderObject(user, selectedChat.users)}>
-                            <IconButton icon={<InfoIcon />} bg='transparent'
-                                _hover={{ bg: 'transparent' }}
-                                _active={{ bg: 'transparent' }}
-                                variant='outline'
-                                color='white' />
-                        </ProfileModal>) :
-                            <ProfileGroupModal chat={selectedChat}>
+                                (
+                                    <>
+                                        <Avatar name={selectedChat.chatName} />
+                                        <Text>
+                                            {selectedChat.chatName.toUpperCase()}
+                                        </Text>
+                                    </>
+                                )
+                            }
+                        </Stack>
+
+                        {
+                            !selectedChat.isGroupChat ? (<ProfileModal user={getSenderObject(user, selectedChat.users)}>
                                 <IconButton icon={<InfoIcon />} bg='transparent'
                                     _hover={{ bg: 'transparent' }}
                                     _active={{ bg: 'transparent' }}
                                     variant='outline'
-                                    color='white'
-                                />
-                            </ProfileGroupModal>
-                    }
+                                    color='white' />
+                            </ProfileModal>) :
+                                <ProfileGroupModal chat={selectedChat}>
+                                    <IconButton icon={<InfoIcon />} bg='transparent'
+                                        _hover={{ bg: 'transparent' }}
+                                        _active={{ bg: 'transparent' }}
+                                        variant='outline'
+                                        color='white'
+                                    />
+                                </ProfileGroupModal>
+                        }
 
+                    </Box>
+
+                    {
+                        !loading ?
+                            (<Box h="100%" bg='gray' m='2px 5px' borderRadius='lg' p='3px 5px' >
+
+                            </Box>
+                            ) : (<Spinner size="xl" alignItems='center' margin='auto' />)
+
+                    }
+                    <FormControl onKeyDown={sendMessage} isRequired>
+                        <InputGroup>
+                            <Input variant='filled' bg='white' placeholder='Type Message...' onChange={typingHandler}
+                                value={newMessage}
+                            />
+                            <InputRightElement>
+                                <ArrowRightIcon onClick={sendMessage} cursor='pointer' color='green.500' />
+                            </InputRightElement>
+                        </InputGroup>
+                    </FormControl>
 
                 </Box > : (
                     <Box
